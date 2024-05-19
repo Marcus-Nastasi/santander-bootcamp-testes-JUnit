4d8dfa07c69c28f960f18064b99d0637686023a5
rolemberg <vinnie.nsts@gmail.com>
2024-05-19 17:48:55 -0300

    implemented People class;

--- src/main/java/com/santander/testes/People.java
+++ src/main/java/com/santander/testes/People.java
@@ -1,4 +1,24 @@
 package com.santander.testes;
 
 public class People {
+
+    private String name;
+    private int age;
+
+    public People(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public boolean isMajor() {
+        return this.age >= 18;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
 }
