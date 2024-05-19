68c2654383f5f9ee1ee642169a5d3963c283fde9
rolemberg <vinnie.nsts@gmail.com>
2024-05-19 19:54:08 -0300

    refactoring to stactic Assertions class importing;

--- src/test/java/PeopleTest.java
+++ src/test/java/PeopleTest.java
@@ -1,31 +1,32 @@
 import com.santander.testes.People;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class PeopleTest {
 
     @Test
     void returnAgeOnInteger() {
         People people = new People("Jessica", "19/05/2000");
-        Assertions.assertEquals(24, people.getAge());
+        assertEquals(24, people.getAge());
     }
 
     @Test
     void returnIfIsMajor() {
         People people = new People("Jessica", "19/05/2000");
-        Assertions.assertTrue(people.isMajor());
+        assertTrue(people.isMajor());
     }
 
     @Test
     void returnIfIsMinor() {
         People people = new People("Leo", "19/05/2010");
-        Assertions.assertFalse(people.isMajor());
+        assertFalse(people.isMajor());
     }
 
     @Test
     void equalsStringsOfBirth() {
         People people = new People("Jessica", "19/05/2000");
-        Assertions.assertEquals("19/05/2000", people.getBirth());
+        assertEquals("19/05/2000", people.getBirth());
     }
 }
 
