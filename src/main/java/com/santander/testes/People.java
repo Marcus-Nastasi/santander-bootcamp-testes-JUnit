a67201cc00ae5cf3eac721d00dd4dd48dd68164c
rolemberg <vinnie.nsts@gmail.com>
2024-05-19 18:20:14 -0300

    refactored People getAge method, and testing with JUnit;

--- src/main/java/com/santander/testes/People.java
+++ src/main/java/com/santander/testes/People.java
@@ -2,17 +2,22 @@ package com.santander.testes;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 public class People {
 
     private String name;
-    private Date birth;
+    private LocalDate birth;
     private final SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
     public People(String name, String birth) throws ParseException {
         this.name = name;
-        this.birth = sdf.parse(birth);
+        this.birth = LocalDate.of(
+            Integer.parseInt(birth.split("/")[2]),
+            Integer.parseInt(birth.split("/")[1]),
+            Integer.parseInt(birth.split("/")[0])
+        );
     }
 
     public String getName() {
@@ -20,11 +25,9 @@ public class People {
     }
 
     public int getAge() {
-        long now = new Date().getTime();
-        long brt = this.birth.getTime();
-        long age = now - brt;
-        return (int) age/10000;
+        return (int) ChronoUnit.YEARS.between(this.birth, LocalDate.now());
     }
 }
+
 
 
--- src/test/java/PeopleTest.java
+++ src/test/java/PeopleTest.java
@@ -5,15 +5,13 @@ import org.junit.jupiter.api.Test;
 public class PeopleTest {
 
     @Test
-    void isAgeMajor() {
-        People people = new People("Jon", 22);
-        Assertions.assertTrue(people.isMajor());
-    }
-
-    @Test
-    void isMinor() {
-        People people1 = new People("Mary", 16);
-        Assertions.assertFalse(people1.isMajor());
+    void returnAgeOnInteger() {
+        try {
+            People people = new People("Jessica", "19/05/2000");
+            Assertions.assertEquals(24, people.getAge());
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
     }
 }
 
