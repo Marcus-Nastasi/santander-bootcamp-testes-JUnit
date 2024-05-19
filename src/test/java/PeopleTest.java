516bb3b3b696d8ff0fd15346917ef1f58463e298
rolemberg <vinnie.nsts@gmail.com>
2024-05-19 17:52:45 -0300

    created first test;

--- src/test/java/PeopleTest.java
+++ src/test/java/PeopleTest.java
@@ -1,4 +1,14 @@
+import com.santander.testes.People;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class PeopleTest {
 
-
+    @Test
+    void isAgeMajor() {
+        People people = new People("Jon", 22);
+        Assertions.assertTrue(people.isMajor());
+    }
 }
+
+
