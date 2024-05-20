4abdf988f65b3da3294b75b63f37952028124b64
rolemberg <vinnie.nsts@gmail.com>
2024-05-20 15:15:15 -0300

    testing manually;

--- src/main/java/com/santander/testes/Main.java
+++ src/main/java/com/santander/testes/Main.java
@@ -4,6 +4,9 @@ public class Main {
     public static void main(String[] args) {
         People people = new People("Mark", "26/10/2001");
         System.out.println(people);
+        System.out.println(people.getBirth());
+        System.out.println(people.getAge());
+        System.out.println(people.isMajor());
     }
 }
 
