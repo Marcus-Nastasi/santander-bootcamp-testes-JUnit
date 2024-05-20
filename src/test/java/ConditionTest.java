b0729e6c2bdbba9031e07ecf241de7f5ec1f2fb7
rolemberg <vinnie.nsts@gmail.com>
2024-05-20 16:38:49 -0300

    enable junt in Linux and Mac, and desable on Windows OS;

--- src/test/java/ConditionTest.java
+++ src/test/java/ConditionTest.java
@@ -1,4 +1,5 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledOnOs;
 import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
 import org.junit.jupiter.api.condition.EnabledOnOs;
 import org.junit.jupiter.api.condition.OS;
@@ -9,7 +10,8 @@ public class ConditionTest {
 
     @Test
     @EnabledIfEnvironmentVariable(named = "USER", matches = "rolemberg")
-    @EnabledOnOs(OS.LINUX)
+    @EnabledOnOs({OS.LINUX, OS.MAC})
+    @DisabledOnOs(OS.WINDOWS)
     void validateJustInUserMark() {
         assertEquals(10, 5+5);
     }
