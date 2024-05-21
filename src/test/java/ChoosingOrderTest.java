0311e9d6d07c5745f5cbb3ae2cf874e49ce45a48
rolemberg <vinnie.nsts@gmail.com>
2024-05-20 21:19:42 -0300

    creating methods and adding orderer;

--- src/test/java/ChoosingOrderTest.java
+++ src/test/java/ChoosingOrderTest.java
@@ -1,19 +1,31 @@
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class ChoosingOrderTest {
 
+    @Order(3)
     @Test
     void testOne() {
-
+        assertTrue(true);
     }
 
+    @Order(2)
     @Test
     void testTwo() {
-
+        assertFalse(false);
     }
 
+    @Order(1)
     @Test
     void testThree() {
-
+        assertEquals(10, 6+4);
     }
 }
+
+
+
