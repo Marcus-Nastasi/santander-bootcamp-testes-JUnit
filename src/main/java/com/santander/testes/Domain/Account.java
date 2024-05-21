b499fa820c6befbfadbb21fa4bf2cd950b19f95b
rolemberg <vinnie.nsts@gmail.com>
2024-05-20 22:35:25 -0300

    creating Account class structure;

--- src/main/java/com/santander/testes/Domain/Account.java
+++ src/main/java/com/santander/testes/Domain/Account.java
@@ -1,4 +1,26 @@
 package com.santander.testes.Domain;
 
 public class Account {
+
+    private final long id;
+    private final int agency;
+    private double balance;
+
+    public Account(long id, int agency, double balance) {
+        this.id = id;
+        this.agency = agency;
+        this.balance = balance;
+    }
+
+    public int getAgency() {
+        return agency;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
 }
