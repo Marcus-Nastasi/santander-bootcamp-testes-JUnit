d165edfe8de038074a20b36e02ec90b3b1e7f534
rolemberg <vinnie.nsts@gmail.com>
2024-05-20 21:17:35 -0300

    creating tests one, two and three;

--- src/test/java/ChoosingOrderTest.java
+++ src/test/java/ChoosingOrderTest.java
@@ -1,4 +1,19 @@
+import org.junit.jupiter.api.Test;
+
 public class ChoosingOrderTest {
 
+    @Test
+    void testOne() {
 
+    }
+
+    @Test
+    void testTwo() {
+
+    }
+
+    @Test
+    void testThree() {
+
+    }
 }
