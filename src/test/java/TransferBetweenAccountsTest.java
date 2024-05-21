e580ac111563d8e4610aa246aa45b0e114b86d03
rolemberg <vinnie.nsts@gmail.com>
2024-05-20 22:45:26 -0300

    creating unit test to transfer method;

--- src/test/java/TransferBetweenAccountsTest.java
+++ src/test/java/TransferBetweenAccountsTest.java
@@ -1,5 +1,6 @@
 import com.santander.testes.Domain.Account;
 import com.santander.testes.Domain.TransferBetweenAccounts;
+import com.santander.testes.Exceptions.AccountException;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,11 +8,19 @@ import static org.junit.jupiter.api.Assertions.*;
 public class TransferBetweenAccountsTest {
 
     @Test
-    void transferingSuccessfuly() {
+    void transferringSuccessfully() {
         Account ac1 = new Account(1111, 1001, 100.00);
         Account ac2 = new Account(1222, 1001, 200.00);
         TransferBetweenAccounts t = new TransferBetweenAccounts(100, ac1, ac2);
         assertDoesNotThrow(() -> t.transfer(100));
+    }
+
+    @Test
+    void transferringNotSuccessfully() {
+        Account ac1 = new Account(1111, 1001, 100.00);
+        Account ac2 = new Account(1222, 1001, 200.00);
+        TransferBetweenAccounts t = new TransferBetweenAccounts(100, ac1, ac2);
+        assertThrows(AccountException.class, () -> t.transfer(0));
     }
 }
 
